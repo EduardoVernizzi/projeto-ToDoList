652fd1d008b957c5c903ddf85c72dca2728ad507
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2025-07-08 11:18:37 -0300

    fix: Ajuste layout FinallCall(footer)

--- src/App.jsx
+++ src/App.jsx
@@ -23,33 +23,44 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1 className="title">To Do List 📝</h1>
+    <>
+      <div className="container">
+        <h1 className="title">To Do List 📝</h1>
 
-      <div className="input-area">
-        <input
-          type="text"
-          placeholder="Digite uma tarefa..."
-          value={tarefa}
-          onChange={(e) => setTarefa(e.target.value)}
-        />
-        <button className="btn-adicionar" onClick={adicionarTarefa}>
-          Adicionar
-        </button>
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="Digite uma tarefa..."
+            value={tarefa}
+            onChange={(e) => setTarefa(e.target.value)}
+          />
+          <button className="btn-adicionar" onClick={adicionarTarefa}>
+            Adicionar
+          </button>
+        </div>
+
+        <ul className="lista-tarefas">
+          {lista.map((item, index) => (
+            <li key={index} className={item.concluida ? 'concluida' : ''}>
+              <span className="texto-tarefa">{item.texto}</span>
+              <div className="botoes">
+                <button onClick={() => toggleConcluida(index)}>  {item.concluida ? '❌' : '✅'}</button>
+                <button onClick={() => excluirTarefa(index)}>🗑️</button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
 
-      <ul className="lista-tarefas">
-        {lista.map((item, index) => (
-          <li key={index} className={item.concluida ? 'concluida' : ''}>
-          <span className="texto-tarefa">{item.texto}</span>
-            <div className="botoes">
-              <button onClick={() => toggleConcluida(index)}>  {item.concluida ? '❌' : '✅'}</button>
-              <button onClick={() => excluirTarefa(index)}>🗑️</button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <footer>
+      <p>
+        &copy; 2025 Carlos Eduardo Vernizzi Silva. Todos os direitos reservados.
+      </p>
+
+      </footer>
+
+
+    </>
   )
 }
 
